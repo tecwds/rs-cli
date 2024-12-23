540eb9c980d9ca8aa07380c55273d42e35a52498
tecwds <tecwds@163.com>
2024-12-23 15:24:01 +0800

    feat: convert any CSV to JSON or YAML

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@ use clap::Parser;
 use rs_cli::{process_csv, Opts, SubCommand};
 
 fn main() -> anyhow::Result<()> {
+    /// 获取命令参数
     let opts = Opts::parse();
 
     match opts.cmd {
-        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+        // csv 相关操作
+        SubCommand::Csv(opts) => {
+            let output = if let Some(output) = &opts.output {
+                output.clone()
+            } else {
+                format!("output.{}", opts.format)
+            };
+
+            // 处理
+            process_csv(&opts.input, &output, opts.format)?
+        }
     }
     Ok(())
 }
--- src/opts.rs
+++ src/opts.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+use std::str::FromStr;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
@@ -18,14 +20,63 @@ pub struct CsvOpts {
     #[arg(short, long, value_parser = verify_input_file)]
     pub input: String,
 
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
+
+    #[arg(short, long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
 
     #[arg(long, default_value_t = true)]
     pub header: bool
+}
+
+#[derive(Debug, Clone)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
+impl TryFrom<&str> for OutputFormat {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => Err(format!("{} is not a valid output format", value)),
+        }
+    }
+}
+
+impl From<OutputFormat> for &'static str {
+
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            _ => anyhow::bail!("{} is not a valid output format", value)
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(self.clone()))
+    }
 }
 
 /// 验证输入的文件是否存在
@@ -35,4 +86,8 @@ fn verify_input_file(filename: &str) -> Result<String, String> {
     } else {
         Err(format!("{} does not exist", filename))
     }
+}
+
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    format.parse::<OutputFormat>()
 }
